241fa5752d68f9a2e9aba511ff843b90f1503a45
Diego Parra <28814932+diegoapb@users.noreply.github.com>
2022-05-27 14:58:52 -0500

    Initial commit

---
+++ test/listen-world-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ListenWorldCdk from '../lib/listen-world-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/listen-world-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ListenWorldCdk.ListenWorldCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
